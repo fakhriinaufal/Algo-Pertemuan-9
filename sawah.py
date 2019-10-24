c56ef8355451f816422adc74eb709d3c668945b8
fakhriinaufal <56950413+fakhriinaufal@users.noreply.github.com>
2019-10-24 18:28:16 +0700

    Create sawah.py
    himasif.in/16n

---
+++ sawah.py
@@ -0,0 +1,15 @@
+panjangLahan = float(input("Masukkan panjang lahan: "))
+lebarLahan = float(input("Masukkan lebar lahan: "))
+pupukBiasanya = int(input("Masukkan jumlah pupuk biasanya: "))
+
+luasLahan = panjangLahan * lebarLahan
+jumlahBibit = (panjangLahan / 0.2) * (lebarLahan / 0.2)
+kebutuhanPupuk = luasLahan * 400
+if pupukBiasanya >= kebutuhanPupuk:
+  print("Jumlah pupuk sudah mencukupi")
+else:
+  tambahanPupuk = kebutuhanPupuk - pupukBiasanya
+print("Luas lahan adalah ", luasLahan, "meter persegi")
+print("Jumlah bibit yang dibutuhkan adalah ", jumlahBibit)
+print("Pupuk yang dibutuhkan dalam sekali pemupukan adalah ", kebutuhanPupuk, "gram")
+print("pupuk yang dibutuhkan sampai panen adalah ", kebutuhanPupuk * 4)
